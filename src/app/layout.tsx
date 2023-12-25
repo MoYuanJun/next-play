9dea4312b4b5043b53a32b01982fb4612f17bac0
MoYuanJun <moyuanjun925@gmail.com>
2023-12-25 11:03:34 +0800

    chore: remove inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +16,7 @@ export default function RootLayout({
     <html
       lang="en"
       className="dark">
-      <body className={inter.className}>
+      <body>
         {children}
         <span>2222</span>
         222

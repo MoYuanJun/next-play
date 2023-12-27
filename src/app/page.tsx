de120b371f709cd38f7f4904e26deba62da31fc6
MoYuanJun <moyuanjun925@gmail.com>
2023-12-27 17:52:28 +0800

    chore: css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import css from './index.module.css';
+
 export default function Home() {
-  return <main className="container">Root Page</main>;
+  return (
+    <main className="container">
+      Root Page
+      <div style={{ color: 'red' }}>style</div>
+      <div className={css.item}>CSS 模块化</div>
+      <div className="g-link">全局样式</div>
+    </main>
+  );
 }

79fa14bab5f7d8ffd53084a21b347006476a18ee
MoYuanJun <moyuanjun925@gmail.com>
2024-03-26 11:09:35 +0800

    删除多余代码

--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '**.{js,ts,jsx,tsx,mjs,cjs}': 'eslint',
-  '**/*.{css,scss,sass,less}': 'stylelint',
-};
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@nextui-org/react';
-
-export default function Home() {
-  return (
-    <main>
-      <Button color="primary">1</Button>
-    </main>
-  );
-}
